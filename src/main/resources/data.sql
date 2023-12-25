086460816a698ac30b8f5e14d68fc848c792cbc1
Ivan Soldatov <38758452+ivansoldatov@users.noreply.github.com>
2023-12-25 21:49:06 +0300

    feat: добавить db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO mpa (id, title)
+VALUES (1,'G'),
+	   (2,'PG'),
+	   (3,'PG13'),
+	   (4,'R'),
+	   (5,'NC');
+
+INSERT INTO genres (id, name)
+VALUES (1,'comedy'),
+	   (2,'drama'),
+	   (3,'cartoon'),
+	   (4,'thriller'),
+	   (5,'action');
+

afa01ab68623828d2c1f8c4019263af828248fb2
Ivan Soldatov <38758452+ivansoldatov@users.noreply.github.com>
2023-12-26 22:03:04 +0300

    inProgress: реализуем InDBUserStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/InDBUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/InDBUserStorage.java
@@ -1,15 +1,16 @@
 package ru.yandex.practicum.filmorate.storage.db;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
-import java.sql.Date;
-import java.sql.Timestamp;
+import java.sql.*;
 import java.util.List;
+import java.util.Objects;
 
 @Component("inDBUserStorage")
 public class InDBUserStorage implements UserStorage {
@@ -23,32 +24,70 @@ public class InDBUserStorage implements UserStorage {
 
     @Override
     public User save(User user) {
-        String sqlQuery = "INSERT INTO Users (email, login, name, birthday) VALUES (?,?,?,?)";
-        jdbcTemplate.update(sqlQuery,
-                user.getEmail(),
-                user.getLogin(),
-                user.getName(),
-                Date.valueOf(user.getBirthday()));
+        String sqlQuery = "INSERT INTO Users (email, login, name, birthday) " +
+                "VALUES (?,?,?,?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sqlQuery, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getEmail());
+            ps.setString(2, user.getLogin());
+            ps.setString(3, user.getName());
+            ps.setDate(4, Date.valueOf(user.getBirthday()));
+            return ps;
+        }, keyHolder);
+
+        user.setId(Objects.requireNonNull(keyHolder.getKey()).intValue());
+
         return user;
     }
 
     @Override
     public User update(User user) {
-        return null;
+        String sqlQuery = "UPDATE Users " +
+                "SET email = ?, " +
+                "login = ?, " +
+                "name = ?, " +
+                "birthday = ? " +
+                "WHERE id = ?";
+
+        jdbcTemplate.update(sqlQuery
+                , user.getEmail()
+                , user.getLogin()
+                , user.getName()
+                , Date.valueOf(user.getBirthday()));
+
+        return user;
     }
 
     @Override
     public boolean delete(int id) {
-        return false;
+        String sqlQuery = "DELETE FROM Users WHERE id = ?";
+        return jdbcTemplate.update(sqlQuery, id) > 0;
     }
 
     @Override
     public User get(int id) {
-        return null;
+        String sqlQuery = "SELECT id, email, login, name, birthday " +
+                "FROM Users " +
+                "WHERE id = ?";
+        return jdbcTemplate.queryForObject(sqlQuery, this::mapRowToUser, id);
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        String sqlQuery = "SELECT id, email, login, name, birthday " +
+                          "FROM Users";
+        return jdbcTemplate.query(sqlQuery, this::mapRowToUser);
+    }
+
+    private User mapRowToUser(ResultSet rs, int rowNum) throws SQLException {
+        return User.builder()
+                .id(rs.getInt("id"))
+                .email(rs.getString("email"))
+                .login(rs.getString("login"))
+                .name(rs.getString("name"))
+                .birthday(rs.getDate("birthday").toLocalDate())
+                .build();
     }
 }
